c0d0ac6f4c45688abdf23b2f18759cfbe3ec319c
AntipasZemba <169179206+AntipasZemba@users.noreply.github.com>
2024-05-24 15:40:59 -0400

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,316 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.txtFirstName = New System.Windows.Forms.TextBox()
+        Me.txtLastName = New System.Windows.Forms.TextBox()
+        Me.txtPhoneNumber = New System.Windows.Forms.TextBox()
+        Me.txtEmail = New System.Windows.Forms.TextBox()
+        Me.txtDays = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.lblSubtotal = New System.Windows.Forms.Label()
+        Me.lblTax = New System.Windows.Forms.Label()
+        Me.lblTotal = New System.Windows.Forms.Label()
+        Me.radOffSeason = New System.Windows.Forms.RadioButton()
+        Me.radPeakSeason = New System.Windows.Forms.RadioButton()
+        Me.radStandard = New System.Windows.Forms.RadioButton()
+        Me.Label12 = New System.Windows.Forms.Label()
+        Me.btnCalculate = New System.Windows.Forms.Button()
+        Me.btnClear = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txtFirstName
+        '
+        Me.txtFirstName.Location = New System.Drawing.Point(166, 35)
+        Me.txtFirstName.Multiline = True
+        Me.txtFirstName.Name = "txtFirstName"
+        Me.txtFirstName.Size = New System.Drawing.Size(78, 17)
+        Me.txtFirstName.TabIndex = 0
+        '
+        'txtLastName
+        '
+        Me.txtLastName.Location = New System.Drawing.Point(166, 58)
+        Me.txtLastName.Multiline = True
+        Me.txtLastName.Name = "txtLastName"
+        Me.txtLastName.Size = New System.Drawing.Size(78, 17)
+        Me.txtLastName.TabIndex = 1
+        '
+        'txtPhoneNumber
+        '
+        Me.txtPhoneNumber.Location = New System.Drawing.Point(166, 81)
+        Me.txtPhoneNumber.Multiline = True
+        Me.txtPhoneNumber.Name = "txtPhoneNumber"
+        Me.txtPhoneNumber.Size = New System.Drawing.Size(92, 17)
+        Me.txtPhoneNumber.TabIndex = 2
+        '
+        'txtEmail
+        '
+        Me.txtEmail.Location = New System.Drawing.Point(166, 104)
+        Me.txtEmail.Multiline = True
+        Me.txtEmail.Name = "txtEmail"
+        Me.txtEmail.Size = New System.Drawing.Size(148, 17)
+        Me.txtEmail.TabIndex = 3
+        '
+        'txtDays
+        '
+        Me.txtDays.Location = New System.Drawing.Point(166, 127)
+        Me.txtDays.Multiline = True
+        Me.txtDays.Name = "txtDays"
+        Me.txtDays.Size = New System.Drawing.Size(45, 17)
+        Me.txtDays.TabIndex = 4
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(45, 39)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(60, 13)
+        Me.Label1.TabIndex = 5
+        Me.Label1.Text = "First Name:"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(45, 62)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(61, 13)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "Last Name:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(45, 85)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(81, 13)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "Phone Number;"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(45, 108)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(76, 13)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "Email Address:"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(45, 131)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(34, 13)
+        Me.Label5.TabIndex = 9
+        Me.Label5.Text = "Days:"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(45, 263)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(49, 13)
+        Me.Label6.TabIndex = 10
+        Me.Label6.Text = "Subtotal:"
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Location = New System.Drawing.Point(45, 288)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(28, 13)
+        Me.Label7.TabIndex = 11
+        Me.Label7.Text = "Tax:"
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Location = New System.Drawing.Point(45, 311)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(68, 13)
+        Me.Label8.TabIndex = 12
+        Me.Label8.Text = "Rental Total:"
+        '
+        'lblSubtotal
+        '
+        Me.lblSubtotal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblSubtotal.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblSubtotal.Location = New System.Drawing.Point(163, 256)
+        Me.lblSubtotal.Name = "lblSubtotal"
+        Me.lblSubtotal.Size = New System.Drawing.Size(81, 20)
+        Me.lblSubtotal.TabIndex = 13
+        '
+        'lblTax
+        '
+        Me.lblTax.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblTax.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTax.Location = New System.Drawing.Point(163, 281)
+        Me.lblTax.Name = "lblTax"
+        Me.lblTax.Size = New System.Drawing.Size(81, 20)
+        Me.lblTax.TabIndex = 14
+        '
+        'lblTotal
+        '
+        Me.lblTotal.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.lblTotal.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblTotal.Location = New System.Drawing.Point(163, 304)
+        Me.lblTotal.Name = "lblTotal"
+        Me.lblTotal.Size = New System.Drawing.Size(81, 20)
+        Me.lblTotal.TabIndex = 15
+        '
+        'radOffSeason
+        '
+        Me.radOffSeason.AutoSize = True
+        Me.radOffSeason.Location = New System.Drawing.Point(166, 210)
+        Me.radOffSeason.Name = "radOffSeason"
+        Me.radOffSeason.Size = New System.Drawing.Size(39, 17)
+        Me.radOffSeason.TabIndex = 16
+        Me.radOffSeason.Text = "Off"
+        Me.radOffSeason.UseVisualStyleBackColor = True
+        '
+        'radPeakSeason
+        '
+        Me.radPeakSeason.AutoSize = True
+        Me.radPeakSeason.Location = New System.Drawing.Point(166, 187)
+        Me.radPeakSeason.Name = "radPeakSeason"
+        Me.radPeakSeason.Size = New System.Drawing.Size(50, 17)
+        Me.radPeakSeason.TabIndex = 17
+        Me.radPeakSeason.Text = "Peak"
+        Me.radPeakSeason.UseVisualStyleBackColor = True
+        '
+        'radStandard
+        '
+        Me.radStandard.AutoSize = True
+        Me.radStandard.Checked = True
+        Me.radStandard.Location = New System.Drawing.Point(166, 164)
+        Me.radStandard.Name = "radStandard"
+        Me.radStandard.Size = New System.Drawing.Size(68, 17)
+        Me.radStandard.TabIndex = 18
+        Me.radStandard.TabStop = True
+        Me.radStandard.Text = "Standard"
+        Me.radStandard.UseVisualStyleBackColor = True
+        '
+        'Label12
+        '
+        Me.Label12.AutoSize = True
+        Me.Label12.Location = New System.Drawing.Point(45, 191)
+        Me.Label12.Name = "Label12"
+        Me.Label12.Size = New System.Drawing.Size(51, 13)
+        Me.Label12.TabIndex = 19
+        Me.Label12.Text = "Seasons:"
+        '
+        'btnCalculate
+        '
+        Me.btnCalculate.Location = New System.Drawing.Point(45, 353)
+        Me.btnCalculate.Name = "btnCalculate"
+        Me.btnCalculate.Size = New System.Drawing.Size(75, 23)
+        Me.btnCalculate.TabIndex = 20
+        Me.btnCalculate.Text = "Calculate"
+        Me.btnCalculate.UseVisualStyleBackColor = True
+        '
+        'btnClear
+        '
+        Me.btnClear.Location = New System.Drawing.Point(140, 353)
+        Me.btnClear.Name = "btnClear"
+        Me.btnClear.Size = New System.Drawing.Size(75, 23)
+        Me.btnClear.TabIndex = 21
+        Me.btnClear.Text = "Clear"
+        Me.btnClear.UseVisualStyleBackColor = True
+        '
+        'btnExit
+        '
+        Me.btnExit.Location = New System.Drawing.Point(239, 353)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(75, 23)
+        Me.btnExit.TabIndex = 22
+        Me.btnExit.Text = "Exit"
+        Me.btnExit.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(362, 410)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnClear)
+        Me.Controls.Add(Me.btnCalculate)
+        Me.Controls.Add(Me.Label12)
+        Me.Controls.Add(Me.radStandard)
+        Me.Controls.Add(Me.radPeakSeason)
+        Me.Controls.Add(Me.radOffSeason)
+        Me.Controls.Add(Me.lblTotal)
+        Me.Controls.Add(Me.lblTax)
+        Me.Controls.Add(Me.lblSubtotal)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txtDays)
+        Me.Controls.Add(Me.txtEmail)
+        Me.Controls.Add(Me.txtPhoneNumber)
+        Me.Controls.Add(Me.txtLastName)
+        Me.Controls.Add(Me.txtFirstName)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Rental Condo"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents txtFirstName As TextBox
+    Friend WithEvents txtLastName As TextBox
+    Friend WithEvents txtPhoneNumber As TextBox
+    Friend WithEvents txtEmail As TextBox
+    Friend WithEvents txtDays As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents lblSubtotal As Label
+    Friend WithEvents lblTax As Label
+    Friend WithEvents lblTotal As Label
+    Friend WithEvents radOffSeason As RadioButton
+    Friend WithEvents radPeakSeason As RadioButton
+    Friend WithEvents radStandard As RadioButton
+    Friend WithEvents Label12 As Label
+    Friend WithEvents btnCalculate As Button
+    Friend WithEvents btnClear As Button
+    Friend WithEvents btnExit As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,101 @@
+﻿Imports System.Diagnostics.Eventing.Reader
+
+Public Class Form1
+    'Displaring cinstant
+    Dim dblOffSeason As Double = 50
+    Dim dblPeakSeason As Double = 150
+    Dim dblStandardSeason As Double = 100
+
+    Private Sub btnExit_Click(sender As Object, e As EventArgs) Handles btnExit.Click
+        ' Closing program
+        Close()
+
+    End Sub
+
+    Private Sub btnClear_Click(sender As Object, e As EventArgs) Handles btnClear.Click
+        ' Clear inputs and outputs
+        txtFirstName.ResetText()
+        txtLastName.ResetText()
+        txtEmail.ResetText()
+        txtPhoneNumber.ResetText()
+        txtDays.ResetText()
+
+        txtFirstName.Focus()
+        radStandard.Checked = True
+
+        radOffSeason.Checked = False
+        radPeakSeason.Checked = False
+
+        lblSubtotal.Text = ""
+        lblTax.Text = ""
+        lblTotal.Text = ""
+
+    End Sub
+
+    Private Sub btnCalculate_Click(sender As Object, e As EventArgs) Handles btnCalculate.Click
+
+        Dim intDays As Integer
+        Dim dblSeasonCost As Double
+        Dim dblTotalDiscount As Double
+
+        Dim dblSubtotal As Double
+        Dim dblTax As Double
+        Dim dblTotal As Double
+
+        'Validate Input
+        If txtPhoneNumber.Text = "" Then
+            MessageBox.Show("Phone number must be entered")
+            txtPhoneNumber.Focus()
+        End If
+
+        If txtEmail.Text = "" Then
+            MessageBox.Show("Email address must be entered")
+            txtEmail.Focus()
+        End If
+
+        If IsNumeric(txtDays.Text) Then
+            intDays = txtDays.Text
+            If intDays < 1 Then
+                MessageBox.Show("Number of days must greater than 0")
+                txtDays.Focus()
+            End If
+        Else
+            MessageBox.Show("Number of days must be numeric")
+            txtDays.Focus()
+        End If
+
+        'calculation
+        If radOffSeason.Checked Then
+            dblSeasonCost = intDays * dblOffSeason
+        End If
+
+        If radPeakSeason.Checked Then
+            dblSeasonCost = intDays * dblPeakSeason
+        End If
+
+        If radStandard.Checked Then
+            dblSeasonCost = intDays * dblStandardSeason
+        End If
+
+        If intDays > 30 Then
+            dblTotalDiscount = (dblSeasonCost * 10) / 100
+            ' dblTotal = dblSubtotal * .1
+        Else
+            If intDays > 14 Then
+                dblTotalDiscount = (dblSeasonCost * 5) / 100
+                ' dblTotal = dblSubtotal * .05
+            End If
+        End If
+
+        dblSubtotal = dblSeasonCost - dblTotalDiscount
+        dblTax = dblSubtotal * 0.1
+        dblTotal = dblSubtotal + dblTax
+
+
+        'display output
+        lblSubtotal.Text = FormatCurrency(dblSubtotal)
+        lblTax.Text = FormatCurrency(dblTax)
+        lblTotal.Text = FormatCurrency(dblTotal)
+
+    End Sub
+End Class
